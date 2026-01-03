ec96c3fab5e1237cde98ff3517641d5a9dd3e6b6
tyhts0829 <tyhts0829@gmail.com>
2026-01-03 21:50:50 +0900

    📝

---
+++ sketch/readme/5.py
@@ -0,0 +1,25 @@
+from grafix import E, G, run
+
+# A4
+CANVAS_WIDTH = 210
+CANVAS_HEIGHT = 297
+
+
+def draw(t):
+    poly = G.text(text="G")
+    e = E.quantize().fill()
+    return e(poly)
+
+
+if __name__ == "__main__":
+    run(
+        draw,
+        background_color=(1.0, 1.0, 1.0),
+        line_thickness=0.001,
+        line_color=(0.0, 0.0, 0.0),
+        render_scale=3.5,
+        canvas_size=(CANVAS_WIDTH, CANVAS_HEIGHT),
+        parameter_gui=True,
+        midi_port_name="Grid",
+        midi_mode="14bit",
+    )
