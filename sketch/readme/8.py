f732843876625baf273ed22d0020d4537a9bab64
tyhts0829 <31405507+tyhts0829@users.noreply.github.com>
2026-01-07 22:30:10 +0900

    📝

--- sketch/readme/8.py
+++ sketch/readme/8.py
@@ -6,6 +6,7 @@ CANVAS_HEIGHT = 297
 
 
 def draw(t):
+    DENSITY_RATIO = 0.9
     t1 = G.text(
         text="g",
         font="GoogleSans-Regular.ttf",
@@ -32,7 +33,7 @@ def draw(t):
 
     t3 = G.text(
         text="g",
-        font="Geist-Medium.ttf",
+        font="Break Light.ttf",
         font_index=0,
         text_align="left",
         letter_spacing_em=0.0,
@@ -70,7 +71,7 @@ def draw(t):
         bypass=False,
         angle_sets=1,
         angle=124.78,
-        density=522.034,
+        density=522.034 * DENSITY_RATIO,
         spacing_gradient=0.339,
         remove_boundary=True,
     )
@@ -79,7 +80,7 @@ def draw(t):
         bypass=False,
         angle_sets=1,
         angle=32.949,
-        density=447.458,
+        density=447.458 * DENSITY_RATIO,
         spacing_gradient=0.0,
         remove_boundary=True,
     )
@@ -88,7 +89,7 @@ def draw(t):
         bypass=False,
         angle_sets=1,
         angle=154.068,
-        density=532.203,
+        density=532.203 * DENSITY_RATIO,
         spacing_gradient=0.0,
         remove_boundary=True,
     )
@@ -97,7 +98,7 @@ def draw(t):
         bypass=False,
         angle_sets=1,
         angle=29.898,
-        density=716.9490000000001,
+        density=716.9490000000001 * DENSITY_RATIO,
         spacing_gradient=0.0,
         remove_boundary=True,
     )
@@ -106,7 +107,7 @@ def draw(t):
         bypass=False,
         angle_sets=1,
         angle=162.915,
-        density=669.492,
+        density=669.492 * DENSITY_RATIO,
         spacing_gradient=0.0,
         remove_boundary=True,
     )
@@ -117,39 +118,216 @@ def draw(t):
     t4 = e4(t4)
     t5 = e5(t5)
 
-    e = E.affine()
+    e = E(name="g_affine").affine(
+        bypass=False,
+        auto_center=True,
+        pivot=(0.0, 0.0, 0.0),
+        rotation=(0.0, 0.0, 0.0),
+        scale=(5.932, 6.0, 6.0),
+        delta=(0.0, 0.0, 0.0),
+    )
 
     t = e(t1 + t2 + t3 + t4 + t5)
 
-    cross = G.line()
-    e_cross = E.repeat().repeat().repeat()
+    cross = G(name="cross").line(
+        center=(20.0, 20.0, 0.0),
+        length=10.0,
+        angle=0.0,
+    )
+
+    e_cross = (
+        E(name="cross_repeats")
+        .repeat(
+            bypass=False,
+            count=1,
+            cumulative_scale=False,
+            cumulative_offset=False,
+            cumulative_rotate=False,
+            offset=(0.0, 0.0, 0.0),
+            rotation_step=(0.0, 0.0, 90.0),
+            scale=(1.0, 1.0, 1.0),
+            curve=1.0,
+            auto_center=True,
+            pivot=(0.0, 0.0, 0.0),
+        )
+        .repeat(
+            bypass=False,
+            count=1,
+            cumulative_scale=False,
+            cumulative_offset=False,
+            cumulative_rotate=False,
+            offset=(170.0, 0.0, 0.0),
+            rotation_step=(0.0, 0.0, 0.0),
+            scale=(1.0, 1.0, 1.0),
+            curve=1.0,
+            auto_center=True,
+            pivot=(0.0, 0.0, 0.0),
+        )
+        .repeat(
+            bypass=False,
+            count=1,
+            cumulative_scale=False,
+            cumulative_offset=False,
+            cumulative_rotate=False,
+            offset=(0.0, 257.0, 0.0),
+            rotation_step=(0.0, 0.0, 0.0),
+            scale=(1.0, 1.0, 1.0),
+            curve=1.0,
+            auto_center=True,
+            pivot=(0.0, 0.0, 0.0),
+        )
+    )
+
     cross = e_cross(cross)
 
-    line = G.line()
-    e_line = E.repeat()
-    line = e_line(line)
+    line_h = G(name="line_h").line(
+        center=(105.0, 20.0, 0.0),
+        length=30.0,
+        angle=0.0,
+    )
 
-    v_line = G.line()
-    e_v_line = E.repeat()
-    v_line = e_v_line(v_line)
+    line_h_repeat = E(name="line_h_repeat").repeat(
+        bypass=False,
+        count=1,
+        cumulative_scale=False,
+        cumulative_offset=False,
+        cumulative_rotate=False,
+        offset=(0.0, 257.0, 0.0),
+        rotation_step=(0.0, 0.0, 0.0),
+        scale=(1.0, 1.0, 1.0),
+        curve=1.0,
+        auto_center=True,
+        pivot=(0.0, 0.0, 0.0),
+    )
 
-    mt1 = G.text()
-    mt2 = G.text()
-    mt3 = G.text()
-    mt4 = G.text()
+    line_h = line_h_repeat(line_h)
+
+    line_v = G(name="line_v").line(
+        center=(20.0, 144.0, 0.0),
+        length=77.0,
+        angle=90.0,
+    )
+
+    line_v_repeat = E(name="line_v_repeat").repeat(
+        bypass=False,
+        count=1,
+        cumulative_scale=False,
+        cumulative_offset=False,
+        cumulative_rotate=False,
+        offset=(170.0, 0.0, 0.0),
+        rotation_step=(0.0, 0.0, 0.0),
+        scale=(1.0, 1.0, 1.0),
+        curve=1.0,
+        auto_center=True,
+        pivot=(0.0, 0.0, 0.0),
+    )
+
+    line_v = line_v_repeat(line_v)
+
+    mt1 = G(name="mt1").text(
+        text="repeat",
+        font="GoogleSans-Regular.ttf",
+        font_index=0,
+        text_align="center",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(35.0, 15.0, 0.0),
+        scale=5.0,
+    )
+
+    mt2 = G(name="mt2").text(
+        text="g",
+        font="GoogleSans-Regular.ttf",
+        font_index=0,
+        text_align="center",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(105.0, 15.0, 0.0),
+        scale=5.0,
+    )
+
+    mt3 = G(name="mt3").text(
+        text="5",
+        font="GoogleSans-Regular.ttf",
+        font_index=0,
+        text_align="center",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(145.0, 15.0, 0.0),
+        scale=5.0,
+    )
+
+    mt4 = G(name="mt4").text(
+        text="times",
+        font="GoogleSans-Regular.ttf",
+        font_index=0,
+        text_align="center",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(178.0, 15.0, 0.0),
+        scale=5.0,
+    )
+
     mt = mt1 + mt2 + mt3 + mt4
-    mt_e = E.fill()
+    mt_e = E(name="mt_e").fill(
+        bypass=False,
+        angle_sets=1,
+        angle=45.0,
+        density=1000.0,
+        spacing_gradient=0.0,
+        remove_boundary=False,
+    )
+
     mt = mt_e(mt)
 
-    box = G.polygon(n_sides=4, phase=45)
-    box_e = E.scale().fill().repeat()
-    box = box_e(box)
+    bar_code = G(name="bar_code").polygon(
+        n_sides=4,
+        phase=45.0,
+        center=(8.0, 207.0, 0.0),
+        scale=1.0,
+    )
 
-    waku = cross + line + v_line + mt
-    e_waku = E.affine()
-    waku = e_waku(waku)
+    bar_code_e = (
+        E(name="bar_code_e")
+        .scale(
+            bypass=False,
+            mode="all",
+            auto_center=True,
+            pivot=(0.0, 0.0, 0.0),
+            scale=(20.0, 6.0, 1.0),
+        )
+        .fill(
+            bypass=False,
+            angle_sets=1,
+            angle=45.0,
+            density=25.0,
+            spacing_gradient=0.0,
+            remove_boundary=False,
+        )
+        .repeat(
+            bypass=False,
+            count=4,
+            cumulative_scale=False,
+            cumulative_offset=False,
+            cumulative_rotate=False,
+            offset=(0.0, 38.0, 0.0),
+            rotation_step=(0.0, 0.0, 0.0),
+            scale=(1.0, 1.0, 1.0),
+            curve=1.0,
+            auto_center=True,
+            pivot=(0.0, 0.0, 0.0),
+        )
+    )
 
-    return t, waku, box
+    bar_code = bar_code_e(bar_code)
+
+    waku = cross + line_h + line_v + mt
+
+    return t, waku, bar_code
 
 
 if __name__ == "__main__":
@@ -161,4 +339,6 @@ if __name__ == "__main__":
         render_scale=3.5,
         canvas_size=(CANVAS_WIDTH, CANVAS_HEIGHT),
         parameter_gui=True,
+        midi_port_name="Grid",
+        midi_mode="14bit",
     )
