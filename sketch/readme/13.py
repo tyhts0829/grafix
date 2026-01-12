694749a852235ac94944e748c13f08353c658844
tyhts0829 <31405507+tyhts0829@users.noreply.github.com>
2026-01-12 17:56:07 +0900

    📝

--- sketch/readme/13.py
+++ sketch/readme/13.py
@@ -1,14 +1,14 @@
 from grafix import E, G, P, run
 
-# A4
-CANVAS_WIDTH = 210
-CANVAS_HEIGHT = 297
+# A5
+CANVAS_WIDTH = 148
+CANVAS_HEIGHT = 210
 
 
 def draw(t):
-    g = G.polyhedron()
-    e = E.scale().fill().subdivide().displace().twist().displace().affine()
-    return e(g), P.layout_guides(canvas_w=CANVAS_WIDTH, canvas_h=CANVAS_HEIGHT)
+    c = G.polygon()
+    e = E.repeat().displace().affine()
+    return e(c)
 
 
 if __name__ == "__main__":
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         background_color=(1.0, 1.0, 1.0),
         line_thickness=0.001,
         line_color=(0.0, 0.0, 0.0),
-        render_scale=3.5,
+        render_scale=5,
         canvas_size=(CANVAS_WIDTH, CANVAS_HEIGHT),
         parameter_gui=True,
         midi_port_name="Grid",
