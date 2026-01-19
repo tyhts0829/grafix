26272f808578d11b11df880f3a31a0043b12ba7d
tyhts0829 <tyhts0829@gmail.com>
2026-01-19 18:42:39 +0900

    📝

--- sketch/readme/18.py
+++ sketch/readme/18.py
@@ -34,21 +34,22 @@ def draw(t):
         canvas_w=297.0,
         canvas_h=210.0,
         axes="both",
-        margin_l=10.0,
-        margin_r=10.0,
-        margin_t=10.0,
-        margin_b=8.935,
+        margin_l=12.0,
+        margin_r=12.0,
+        margin_t=12.0,
+        margin_b=12.0,
         show_center=False,
         cols=8,
         rows=5,
-        gutter_x=2.0,
-        gutter_y=3.608,
+        gutter_x=6.0,
+        gutter_y=6.0,
         show_column_centers=False,
         show_baseline=False,
-        baseline_step=2.672,
+        baseline_step=4.215,
         baseline_offset=0.0,
         offset=(0.0, 0.0, 0.0),
     )
+
     l1 = L(name="l1", geometry_or_list=l1)
 
     # ====================================================================
@@ -61,7 +62,7 @@ def draw(t):
         letter_spacing_em=0.0,
         line_height=1.2,
         quality=0.5,
-        center=(10.236, 29.67, -0.8240000000000001),
+        center=(11.413, 31, -0.8240000000000001),
         scale=15.354000000000001,
     )
 
@@ -85,7 +86,7 @@ def draw(t):
         letter_spacing_em=0.0,
         line_height=1.2,
         quality=0.5,
-        center=(10.236, 44.505, 0.0),
+        center=(11.413, 44.505, 0.0),
         scale=15.354000000000001,
     )
     e_title2 = E.fill(
@@ -108,7 +109,7 @@ def draw(t):
         letter_spacing_em=0.0,
         line_height=1.2,
         quality=0.5,
-        center=(11.413, 14.674, 0.0),
+        center=(11.9, 16.304000000000002, 0.0),
         scale=4.237,
     )
 
@@ -124,7 +125,254 @@ def draw(t):
     title3 = e_title3(title3)
     title_black = L(name="title_black", geometry_or_list=[title1])
     title_gray = L(name="title_gray", geometry_or_list=[title2, title3])
-    return (l1, title_black, title_gray)
+
+    # ====================================================================
+    g = G.polyhedron(
+        activate=True,
+        type_index=3,
+        center=(45.652, 159.78300000000002, 0.0),
+        scale=62.887,
+    )
+
+    e1 = E.rotate(
+        activate=True,
+        auto_center=True,
+        pivot=(0.0, 0.0, 0.0),
+        rotation=(53.407000000000004, 67.253, 0.0),
+    )
+
+    e2 = E.translate(
+        activate=True,
+        delta=(68.478, 0.0, 0.0),
+    ).fill(
+        activate=True,
+        angle_sets=1,
+        angle=45.0,
+        density=44.674,
+        spacing_gradient=0.0,
+        remove_boundary=False,
+    )
+
+    e3 = (
+        E.translate(
+            activate=True,
+            delta=(71.429, 0.0, 0.0),
+        )
+        .subdivide(
+            activate=True,
+            subdivisions=3,
+        )
+        .displace(
+            activate=True,
+            amplitude=(8.0, 8.0, 8.0),
+            spatial_freq=(0.04, 0.04, 0.04),
+            amplitude_gradient=(0.0, 0.0, 0.0),
+            frequency_gradient=(0.0, 0.0, 0.0),
+            gradient_center_offset=(0.0, 0.0, 0.0),
+            min_gradient_factor=0.1,
+            max_gradient_factor=2.0,
+            t=0.0,
+        )
+    )
+
+    e4 = (
+        E.affine(
+            activate=True,
+            auto_center=True,
+            pivot=(0.0, 0.0, 0.0),
+            rotation=(0.0, 0.0, 0.0),
+            scale=(1.0, 1.0, 1.0),
+            delta=(142.308, 0.0, 0.0),
+        )
+        .subdivide(
+            activate=True,
+            subdivisions=3,
+        )
+        .displace(
+            activate=True,
+            amplitude=(8.0, 8.0, 8.0),
+            spatial_freq=(0.04, 0.04, 0.04),
+            amplitude_gradient=(0.0, 0.0, 0.0),
+            frequency_gradient=(0.0, 0.0, 0.0),
+            gradient_center_offset=(0.0, 0.0, 0.0),
+            min_gradient_factor=0.1,
+            max_gradient_factor=2.0,
+            t=0.031,
+        )
+    )
+
+    g1 = e1(g)
+    g2 = e2(g1)
+    g3 = e3(g2)
+    g4 = e4(g2)
+    g = L(name="polyhedrons", geometry_or_list=[g1, g2, g3, g4])
+
+    # ====================================================================
+    text1 = G.text(
+        activate=True,
+        text="Principal1",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(11.413, 92.935, 0.0),
+        scale=4.0,
+    )
+
+    e_text1 = E.fill(
+        activate=True,
+        angle_sets=1,
+        angle=45.0,
+        density=250.0,
+        spacing_gradient=0.0,
+        remove_boundary=False,
+    )
+
+    text1 = e_text1(text1)
+
+    text2 = G.text(
+        activate=True,
+        text="Principal2",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(81.522, 92.935, 0.0),
+        scale=4.0,
+    )
+
+    e_text2 = E.fill(
+        activate=True,
+        angle_sets=1,
+        angle=45.0,
+        density=250.0,
+        spacing_gradient=0.0,
+        remove_boundary=False,
+    )
+
+    text2 = e_text2(text2)
+
+    text3 = G.text(
+        activate=True,
+        text="Principal3",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(151.63, 92.935, 0.0),
+        scale=4.0,
+    )
+
+    e_text3 = E.fill(
+        activate=True,
+        angle_sets=1,
+        angle=45.0,
+        density=250.0,
+        spacing_gradient=0.0,
+        remove_boundary=False,
+    )
+
+    text3 = e_text3(text3)
+
+    text4 = G.text(
+        activate=True,
+        text="Principal4",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(221.0, 92.935, 0.0),
+        scale=4.0,
+    )
+
+    e_text4 = E.fill(
+        activate=True,
+        angle_sets=1,
+        angle=45.0,
+        density=250.0,
+        spacing_gradient=0.0,
+        remove_boundary=False,
+    )
+
+    text4 = e_text4(text4)
+
+    text = L(name="Principals", geometry_or_list=[text1, text2, text3, text4])
+
+    # ====================================================================
+    discription1 = G(name="Discription1").text(
+        activate=True,
+        text="G.polyhedron()\nCreate 3D primitive geometry.",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(11.5, 98.0, 0.0),
+        scale=4.0,
+    )
+
+    e_discription1 = E.fill(density=450.0)
+    discription1 = e_discription1(discription1)
+    discription2 = G(name="Discription2").text(
+        activate=True,
+        text="E,fill()\nHatching geometry with parallel\nlines to express surface.",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(81.5, 98.0, 0.0),
+        scale=4.0,
+    )
+
+    e_discription2 = E.fill(density=450.0)
+    discription2 = e_discription2(discription2)
+    discription3 = G(name="Discription3").text(
+        activate=True,
+        text="E.subdivide().displace()\nSubdivide line to express smooth\ncurves and displace to add noise.",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(151.648, 98.0, 0.0),
+        scale=4.0,
+    )
+
+    e_discription3 = E.fill(density=450.0)
+    discription3 = e_discription3(discription3)
+    discription4 = G(name="Discription4").text(
+        activate=True,
+        text="E.displace(t=t)\nAnimate displacement over time.",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        quality=0.5,
+        center=(221.0, 98.0, 0.0),
+        scale=4.0,
+    )
+
+    e_discription4 = E.fill(density=450.0)
+    discription4 = e_discription4(discription4)
+    discription = L(
+        name="Discription",
+        geometry_or_list=[discription1, discription2, discription3, discription4],
+    )
+
+    return (l1, title_black, title_gray, g, text, discription)
 
 
 if __name__ == "__main__":
