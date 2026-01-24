3123a4813aaf1efbb436f97f5d880452739e6c5a
tyhts0829 <tyhts0829@gmail.com>
2026-01-24 21:17:00 +0900

    📝

--- sketch/readme/examples/1.py
+++ sketch/readme/examples/1.py
@@ -7,15 +7,15 @@ CANVAS_HEIGHT = 210
 
 def draw(t):
     # =========== Layouts ===========
-    layout = P(name="layout1").layout_grid_system(
+    layout = P(name="layout").layout_grid_system(
         activate=True,
         canvas_w=148.0,
         canvas_h=210.0,
         axes="both",
-        margin_l=8.0,
-        margin_r=8.0,
-        margin_t=8.0,
-        margin_b=8.0,
+        margin_l=12.0,
+        margin_r=12.0,
+        margin_t=12.0,
+        margin_b=12.0,
         show_center=False,
         cols=5,
         rows=8,
@@ -23,15 +23,21 @@ def draw(t):
         gutter_y=4.0,
         show_column_centers=False,
         show_baseline=False,
-        baseline_step=8.417,
+        baseline_step=3.959,
         baseline_offset=0.0,
         offset=(0.0, 0.0, 0.0),
     )
 
-    layout = L(name="layout1").layer(layout)
+    layout = L(name="layout").layer(layout)
 
     # ====================================================================
-    line = G.line()
+    line = G(name="separate_line").line(
+        activate=True,
+        center=(11.5, 174.5, 0.0),
+        anchor="left",
+        length=124.5,
+        angle=0.0,
+    )
 
     # ====================================================================
     c = G(name="circle").polygon(
@@ -39,6 +45,7 @@ def draw(t):
         n_sides=565,
         phase=0.0,
         sweep=360.0,
+        center=(75.824, 80.769, 0.0),
         scale=111.684,
     )
 
@@ -78,11 +85,114 @@ def draw(t):
         )
     )
 
+    c = e(c)
     # ====================================================================
-    description = G(name="description").text()
-    e_description = E(name="e_description").fill()
-    description = e_description(description)
-    return layout, e(c), description, line
+    title = G(name="title").text(
+        activate=True,
+        text="Grafix\nResearch",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=0.966,
+        use_bounding_box=False,
+        quality=0.5,
+        center=(11.538, 178.022, 0.0),
+        scale=8.0,
+    )
+
+    e_title = E(name="e_title").fill(
+        activate=True,
+        angle_sets=1,
+        angle=45.0,
+        density=838.488,
+        spacing_gradient=0.0,
+        remove_boundary=False,
+    )
+
+    title = e_title(title)
+
+    number = G(name="number").text(
+        activate=True,
+        text="1",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        use_bounding_box=False,
+        quality=0.5,
+        center=(63.0, 178.022, 0.0),
+        scale=5.0,
+    )
+
+    e_number = E(name="e_number").fill(
+        activate=True,
+        angle_sets=1,
+        angle=45.0,
+        density=35.0,
+        spacing_gradient=0.0,
+        remove_boundary=False,
+    )
+
+    number = e_number(number)
+
+    explanation = G(name="explanation").text(
+        activate=True,
+        text="polygon -> repeat -> displace",
+        font="Helvetica.ttc",
+        font_index=0,
+        text_align="left",
+        letter_spacing_em=0.0,
+        line_height=1.2,
+        use_bounding_box=False,
+        quality=0.5,
+        center=(89.011, 178.022, 0.0),
+        scale=3.0,
+    )
+
+    e_explanation = E(name="e_explanation").fill(
+        activate=True,
+        angle_sets=1,
+        angle=45.0,
+        density=572.165,
+        spacing_gradient=0.0,
+        remove_boundary=False,
+    )
+
+    explanation = e_explanation(explanation)
+
+    bars = G(name="bars").polygon(
+        activate=True,
+        n_sides=4,
+        phase=45.0,
+        sweep=360.0,
+        center=(24.725, 197.5, 0.0),
+        scale=5.155,
+    )
+
+    e_bars = (
+        E(name="e_bars")
+        .scale(
+            activate=True,
+            mode="all",
+            auto_center=True,
+            pivot=(0.0, 0.0, 0.0),
+            scale=(5.824, 0.22, 1.0),
+        )
+        .fill(
+            activate=True,
+            angle_sets=1,
+            angle=45.0,
+            density=97.938,
+            spacing_gradient=0.0,
+            remove_boundary=False,
+        )
+    )
+
+    bars = e_bars(bars)
+
+    return layout, c, line, title, number, explanation, bars, P.axes()
 
 
 if __name__ == "__main__":
