0318bf8a1a08dc44c20c2a9d8f89710420b07175
tyhts0829 <31405507+tyhts0829@users.noreply.github.com>
2026-01-25 21:48:36 +0900

    📝

---
+++ sketch/readme/grn/9.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from grafix import E, G, P, run
+
+# A5
+CANVAS_WIDTH = 148
+CANVAS_HEIGHT = 210
+
+
+def draw(t):
+    frame = P.grn_a5_frame(number_text=str(Path(__file__).stem))
+    g1 = G.polyhedron()
+    e1 = E.rotate().fill().collapse()
+    g1 = e1(g1)
+    return frame, g1
+
+
+if __name__ == "__main__":
+    run(
+        draw,
+        background_color=(1.0, 1.0, 1.0),
+        line_thickness=0.001,
+        line_color=(0.0, 0.0, 0.0),
+        render_scale=5,
+        canvas_size=(CANVAS_WIDTH, CANVAS_HEIGHT),
+        parameter_gui=True,
+        midi_port_name="auto",
+        midi_mode="14bit",
+    )
