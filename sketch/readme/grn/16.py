495449bd62f6e0f15e556820a17cf452e632f864
tyhts0829 <31405507+tyhts0829@users.noreply.github.com>
2026-02-02 23:59:24 +0900

    📝

--- sketch/readme/grn/16.py
+++ sketch/readme/grn/16.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 
 from grafix import E, G, P, run
@@ -9,30 +10,58 @@ CANVAS_HEIGHT = 210
 
 def draw(t):
     frame = P.grn_a5_frame(number_text=str(Path(__file__).stem))
-    g = G.text()
+    g = G.polygon(
+        activate=True,
+        n_sides=128,
+        phase=45.0,
+        sweep=360.0,
+        center=(74.0, 87.363, 0.0),
+        scale=3.093,
+    )
 
-    e = E.isocontour().lowpass().fill()
+    e = (
+        E.isocontour(
+            activate=True,
+            spacing=0.46399999999999997,
+            phase=-8.007,
+            max_dist=55.669999999999995,
+            mode="outside",
+            grid_pitch=1.142,
+            gamma=2.8699999999999997,
+            level_step=1,
+            auto_close_threshold=0.001,
+            keep_original=True,
+        )
+        .lowpass(
+            activate=True,
+            step=2.459,
+            sigma=4.742,
+            closed="auto",
+        )
+        .fill(
+            activate=True,
+            angle_sets=1,
+            angle=45.0,
+            density=408.935,
+            spacing_gradient=0.0,
+            remove_boundary=False,
+        )
+    )
+
     g = e(g)
-
-    # e = E.lens()
-    # poly = G.polygon()
-    # g = e(g, poly)
-
-    # e = E.sdf_attract()
-    # g = e(g, poly)
-
     return frame, g
 
 
 if __name__ == "__main__":
     run(
         draw,
-        background_color=(1.0, 1.0, 1.0),
+        background_color=(0.0, 0.0, 0.0),
         line_thickness=0.001,
-        line_color=(0.0, 0.0, 0.0),
+        line_color=(1.0, 1.0, 1.0),
         render_scale=5,
         canvas_size=(CANVAS_WIDTH, CANVAS_HEIGHT),
         parameter_gui=True,
-        midi_port_name="auto",
+        midi_port_name="Grid",
         midi_mode="14bit",
+        fps=24,
     )
