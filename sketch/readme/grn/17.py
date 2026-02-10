ac9d41c316f02e5745ffbc0db9f3194c2998710b
tyhts0829 <tyhts0829@gmail.com>
2026-02-11 02:17:32 +0900

    📝

--- sketch/readme/grn/17.py
+++ sketch/readme/grn/17.py
@@ -9,10 +9,31 @@ CANVAS_HEIGHT = 210
 
 def draw(t):
     frame = P.grn_a5_frame(number_text=str(Path(__file__).stem))
-    g = G.grid()
-    e = E.buffer().buffer().fill()
-    g = e(g)
-    return g
+    g = G.polyhedron(
+        activate=True,
+        type_index=1,
+        center=(76.63, 132.065, 0.0),
+        scale=82.034,
+    )
+
+    e = E.rotate(
+        activate=True,
+        auto_center=True,
+        rotation=(90.0, -39.13, 127.15),
+    ).repeat(
+        activate=True,
+        layout="grid",
+        count=1,
+        cumulative_scale=False,
+        cumulative_offset=False,
+        cumulative_rotate=False,
+        offset=(0.0, -76.087, 0.0),
+        rotation_step=(-48.913000000000004, -115.435, 40.645),
+        scale=(1.0, 1.0, 1.0),
+        auto_center=True,
+    )
+
+    return e(g), frame
 
 
 if __name__ == "__main__":
