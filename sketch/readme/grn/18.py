ae6396b1caeb3765ee89c484518f4e157d2c89a8
tyhts0829 <tyhts0829@gmail.com>
2026-02-16 23:08:21 +0900

    📝

--- sketch/readme/grn/18.py
+++ sketch/readme/grn/18.py
@@ -1,34 +1,50 @@
-from __future__ import annotations
-
 from pathlib import Path
 
-import numpy as np
-from numba import njit
-
-from grafix import E, G, P, run
-from grafix.api import primitive
-from grafix.core.parameters.meta import ParamMeta
-from grafix.core.realized_geometry import RealizedGeometry
+from grafix import G, L, P, cc, run
 
 # A5
 CANVAS_WIDTH = 148
 CANVAS_HEIGHT = 210
 
+# CONST
+N_ROWS = 6
+N_COLS = 4
+
 
 def draw(t):
-    g = G.text()
-    e = E.fill()
-    g = e(g)
     frame = P.grn_a5_frame(
         activate=True,
         show_layout=False,
-        layout_color_rgb255=(191, 191, 191),
-        number_text=str(Path(__file__).stem),
-        explanation_density=500.0,
+        number_text=Path(__file__).stem,
+        explanation_text="G.lissajous()",
+        explanation_density=450.0,
         template_color_rgb255=(255, 255, 255),
     )
 
-    return g, frame
+    gs = []
+    # WHITE_SPACEの幅、高さをccで制御できるようにする
+    WS_X = cc[1] * 50.0  # 0.0 ~ 20.0
+    WS_Y = cc[2] * 50.0  # 0.0
+    OFFSET_Y = (cc[3] - 0.5) * 50
+
+    # coefs
+    coef_a = int(cc[4] * 10) + 1  # 1 ~ 11
+    coef_b = int(cc[5] * 10) + 1  #
+
+    for i in range(N_COLS):
+        for j in range(N_ROWS):
+            cx = WS_X + (i + 0.5) * (CANVAS_WIDTH - 2 * WS_X) / N_COLS
+            cy = WS_Y + (j + 0.5) * (CANVAS_HEIGHT - 2 * WS_Y) / N_ROWS + OFFSET_Y
+            g = G.lissajous(
+                center=(cx, cy, 0.0),
+                a=j + coef_a,
+                b=i + coef_b,
+                samples=10,
+            )
+            gs.append(g)
+    gs = L.layer(gs, color=(1.0, 1.0, 1.0))
+
+    return gs, frame
 
 
 if __name__ == "__main__":
